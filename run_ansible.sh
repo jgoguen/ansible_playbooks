a0227772e807d63f8191fdd3c918788acd7abde8
Joel Goguen <jgoguen@users.noreply.github.com>
2022-01-26 19:38:08 -0800

    Update setup and run scripts

--- run_ansible.sh
+++ run_ansible.sh
@@ -3,7 +3,7 @@
 
 set -ux
 
-UNAME_BIN="$(command -pv uname)"
+UNAME_BIN="$(command -v uname)"
 if [ -z "${UNAME_BIN}" ]; then
 	printf 'Required tool uname not found\n' >&2
 	exit 1
@@ -16,7 +16,7 @@ else
 	SUDO_CMD=/usr/bin/sudo
 fi
 
-ANSIBLE_GALAXY_BIN="$(command -pv ansible-galaxy)"
+ANSIBLE_GALAXY_BIN="$(command -v ansible-galaxy)"
 if [ -z "${ANSIBLE_GALAXY_BIN}" ]; then
 	printf 'Required tool ansible-galaxy not found\n'
 	exit 1
@@ -27,7 +27,7 @@ if [ "${OSTYPE}" = "darwin" ]; then
 else
 	ROOT_HOME="$(/usr/bin/getent passwd root | /usr/bin/cut -d ':' -f 6)"
 fi
-for collection in ansible.posix kewlfft.aur community.general; do
+for collection in ansible.posix kewlfft.aur community.general community.postgresql; do
 	${SUDO_CMD} test -d "${ROOT_HOME}/.ansible/collections/ansible_collections/$(printf '%s' "${collection}" | tr '.' '/')"; rv=$?
 	if [ "${rv}" -ne 0 ]; then
 		${SUDO_CMD} "${ANSIBLE_GALAXY_BIN}" collection install "${collection}"
--- setup.sh
+++ setup.sh
@@ -1,30 +1,57 @@
 #!/bin/sh
-# vim: set noexpandtab:
+# vim: set noexpandtab tabstop=2 shiftwidth=2 autoindent:
+# vim: set foldmarker=[[[,]]] foldmethod=marker foldlevel=0:
 
 set -eux
 
+# 1Password hostname and email as parameters [[[
 OP_ADDR="${1:-""}"
 OP_EMAIL="${2:-""}"
 
 if [ -z "${OP_ADDR}" ] || [ -z "${OP_EMAIL}" ]; then
-	printf 'Usage: %s op_hostname op_email\n' "${0}"
+	printf 'Usage: %s 1password_hostname 1password_email [--check]\n' "${0}" >&2
 	exit 1
 fi
+shift 2
+# ]]]
 
+# Define cleanup on script exit or termination [[[
 cleanup() {
 	if [ -d "${TEMPDIR}" ]; then
 		/bin/rm -rf "${TEMPDIR}"
 	fi
 
+	if [ -f "${CHEZMOI_FILTER_FILE:-/.does_not_exist}" ]; then
+		/bin/rm -f "${CHEZMOI_FILTER_FILE}"
+	fi
+
 	trap - EXIT
 }
 trap cleanup EXIT INT TERM HUP
+# ]]]
 
+# Determine machine architecture and OS type [[[
 UNAME_BIN="$(command -pv uname)"
 if [ -z "${UNAME_BIN}" ]; then
 	printf 'Required tool uname not found\n' >&2
 	exit 1
 fi
+
+MACHINE="$("${UNAME_BIN}" -m)"
+case "${MACHINE}" in
+	x86_64)
+		MACHINE="amd64"
+		HOMEBREW_BIN="/usr/local/bin/brew"
+		;;
+	arm*)
+		MACHINE="arm"
+		HOMEBREW_BIN="/opt/homebrew/bin/brew"
+		;;
+	aarch64)
+		MACHINE="arm64"
+		HOMEBREW_BIN="/opt/homebrew/bin/brew"
+		;;
+esac
 
 OSTYPE="$("${UNAME_BIN}" | /usr/bin/tr '[:upper:]' '[:lower:]')"
 if [ "${OSTYPE}" = "openbsd" ]; then
@@ -32,7 +59,9 @@ if [ "${OSTYPE}" = "openbsd" ]; then
 else
 	SUDO_CMD=/usr/bin/sudo
 fi
+# ]]]
 
+# Install packages required for setup [[[
 if [ "${OSTYPE}" = "darwin" ]; then
 	if ! xcode-select --print-path >/dev/null 2>&1; then
 		printf 'Installing XCode CLI tools\n'
@@ -50,81 +79,83 @@ if [ "${OSTYPE}" = "darwin" ]; then
 	fi
 
 	if [ ! -x /usr/local/bin/brew ]; then
-		/bin/bash -c "$(${CURL_BIN} -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 	fi
 
-	/usr/local/bin/brew install ansible git git-crypt
+	${HOMEBREW_BIN} install ansible git git-crypt
 elif [ -f /etc/fedora-release ] || [ -f /etc/redhat-release ]; then
-	${SUDO_CMD} /usr/bin/dnf install --refresh -y ansible curl git-core git-crypt libxml2 lsb unzip
-elif [ -f /etc/arch-release ]; then
-	${SUDO_CMD} /usr/bin/pacman -Sy --needed ansible curl git git-crypt libxml2 unzip
+	${SUDO_CMD} /usr/bin/dnf install --refresh -y ansible curl git-core \
+		git-crypt gnupg2 libxml2 lsb unzip
 elif [ -f /etc/debian_version ]; then
-	${SUDO_CMD} /usr/bin/apt install -y ansible curl git git-crypt libxml2-utils unzip
+	${SUDO_CMD} /usr/bin/apt install -y ansible curl git git-crypt gnupg2 \
+		libxml2-utils lsb-release unzip
 elif [ "${OSTYPE}" = "openbsd" ]; then
-	${SUDO_CMD} /usr/sbin/pkg_add -ru ansible curl git git-crypt gnupg libxml unzip
+	${SUDO_CMD} /usr/sbin/pkg_add -ru ansible curl git git-crypt gnupg--%gnupg2 \
+		libxml unzip--
 fi
+# ]]]
 
-ANSIBLE_GALAXY_BIN="$(command -pv ansible-galaxy)"
+# Locate other tools on the system [[[
+ANSIBLE_GALAXY_BIN="$(command -v ansible-galaxy)"
 if [ -z "${ANSIBLE_GALAXY_BIN}" ]; then
 	printf 'Required tool ansible-galaxy not found\n'
 	exit 1
 fi
 
-CHOWN_BIN="$(command -pv chown)"
+CHOWN_BIN="$(command -v chown)"
 if [ -z "${CHOWN_BIN}" ]; then
 	printf 'Required tool chown not found\n' >&2
 	exit 1
 fi
 
-CURL_BIN="$(command -pv curl)"
+CURL_BIN="$(command -v curl)"
 if [ -z "${CURL_BIN}" ]; then
 	printf 'Required tool curl not found\n' >&2
 	exit 1
 fi
 
-MKTEMP_BIN="$(command -pv mktemp)"
+GPG_BIN="$(command -v gpg)"
+if [ -z "${GPG_BIN}" ]; then
+	printf 'Required tool gpg not found\n' >&2
+	exit 1
+fi
+
+MKTEMP_BIN="$(command -v mktemp)"
 if [ -z "${MKTEMP_BIN}" ]; then
 	printf 'Required tool mktemp not found\n' >&2
 	exit 1
 fi
 
-TAR_BIN="$(command -pv tar)"
+TAR_BIN="$(command -v tar)"
 if [ -z "${TAR_BIN}" ]; then
 	printf 'Required tool tar not found\n' >&2
 	exit 1
 fi
 
-UNZIP_BIN="$(command -pv unzip)"
+UNZIP_BIN="$(command -v unzip)"
 if [ -z "${UNZIP_BIN}" ]; then
 	printf 'Required tool unzip not found\n' >&2
 	exit 1
 fi
 
-XMLLINT_BIN="$(command -pv xmllint)"
+XMLLINT_BIN="$(command -v xmllint)"
 if [ -z "${XMLLINT_BIN}" ]; then
 	printf 'Required tool xmllint not found\n' >&2
 	exit 1
 fi
+# ]]]
 
-MACHINE="$("${UNAME_BIN}" -m)"
 TEMPDIR="$("${MKTEMP_BIN}" -d XXXXXXXXXXXXXXXX)"
 
-case "${MACHINE}" in
-	x86_64)
-		MACHINE="amd64"
-		;;
-	arm*)
-		MACHINE="arm"
-		;;
-	aarch64)
-		MACHINE="arm64"
-		;;
-esac
-
+# Install the OpenBSD ports tree if needed [[[
 if [ "${OSTYPE}" = "openbsd" ] && [ ! -d /usr/ports ]; then
 	${CURL_BIN} "https://cdn.openbsd.org/pub/OpenBSD/$("${UNAME_BIN}" -r)/ports.tar.gz" | ${SUDO_CMD} "${TAR_BIN}" zxphf - -C /usr
 fi
+# ]]]
 
+# Fetch the op CLI from upstream and install it [[[
+# This is janky as fuck screen-scraping to get the package URL, here's hoping
+# AgileBits doesn't decide to make minor changes to the page!
 if [ ! -f /usr/local/bin/op ]; then
 	OP_URL="$(${CURL_BIN} -fsSL https://app-updates.agilebits.com/product_history/CLI 2>/dev/null | ${XMLLINT_BIN} --html --dtdattr --xpath "string(//article[not(@class='beta')][1]/div[@class='cli-archs']/p[@class='system ${OSTYPE}']/a[text()='${MACHINE}']/@href)" - 2>/dev/null)"
 	${CURL_BIN} -fsSL "${OP_URL}" >"${TEMPDIR}/op.zip"
@@ -137,22 +168,17 @@ if [ ! -f /usr/local/bin/op ]; then
 		${SUDO_CMD} /bin/mv ./op /usr/local/bin/op
 	fi
 fi
+# ]]]
 
 eval "$(/usr/local/bin/op signin "${OP_ADDR}" "${OP_EMAIL}")"
 
-if [ ! -d "${HOME}/.ssh" ]; then
-	/bin/mkdir "${HOME}/.ssh"
-	/bin/chmod 0700 "${HOME}/.ssh"
+/usr/local/bin/op get document qukaq3aej2hftq6t2ojuwvpm6m | ${GPG_BIN} --import
+
+if [ ! -d /var/ansible_playbooks ]; then
+	$(command -v git) clone --depth 1 https://github.com/jgoguen/ansible_playbooks.git
+	${SUDO_CMD} /bin/mv ansible_playbooks /var/
+	cd /var/ansible_playbooks
+	$(command -v git-crypt) unlock
 fi
-/usr/local/bin/op get document g5e5zo2sgpkwum6npqbt6l7ari >"${HOME}/.ssh/github"
-/bin/chmod 0600 "${HOME}"/.ssh/*
-/usr/bin/ssh-keygen -y -f "${HOME}/.ssh/github" >"${HOME}/.ssh/github.pub"
 
-/usr/local/bin/op get document qukaq3aej2hftq6t2ojuwvpm6m | gpg --import
-
-/usr/bin/ssh-agent /bin/sh -c "/usr/bin/ssh-add \"${HOME}/.ssh/github\"; $(command -v git) clone git@github.com:jgoguen/ansible_playbooks.git"
-${SUDO_CMD} /bin/mv ansible_playbooks /var/
-cd /var/ansible_playbooks
-$(command -v git-crypt) unlock
-
-/bin/sh /var/ansible_playbooks/run_ansible.sh
+/bin/sh /var/ansible_playbooks/run_ansible.sh "$@"
